a9b3c638c449dd88f2cd854d46fb3a57382f0769
KosuruB <bkosuru@pnw.edu>
2025-02-10 20:10:14 +0000

    Forked professors code and modified it

--- CICD_CLI/Program.cs
+++ CICD_CLI/Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using CICD_Core;
+
+int a, b;
+
+Console.WriteLine("Enter the numerator: ");
+while (true)
+{
+    string? a1 = (Console.ReadLine());
+    if (!int.TryParse(a1, out a))
+    {
+        Console.WriteLine("Please enter an integer!");
+    }
+    else break;
+}
+
+
+Console.WriteLine("Enter the denominator: ");
+while (true)
+{
+    string? b1 = (Console.ReadLine());
+    if (!int.TryParse(b1, out b))
+    {
+        Console.WriteLine("Please enter an integer!");
+    }
+    else break;
+}
+
+
+double result = Calculator.Divide(a, b);
+Console.WriteLine($"The result of {a} / {b} = {result}");
---
+++ CICD_Core/Calculator.cs
@@ -0,0 +1,14 @@
+﻿namespace CICD_Core
+{
+    public class Calculator
+    {
+        public static double Divide(int a, int b)
+        {
+
+            if (b == 0) { throw new DivideByZeroException("Denominator cannot be 0"); }
+
+            return (double)a/b;
+        }
+
+    }
+}
