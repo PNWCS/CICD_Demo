c5829b56e593de930a0a7fd8bb8002d90eb0afbd
Mohammed-Shoaib124 <151385351+Mohammed-Shoaib124@users.noreply.github.com>
2025-02-10 20:27:24 +0000

    Updated the test cases fore divide function

--- CICD_CLI/Program.cs
+++ CICD_CLI/Program.cs
@@ -1,10 +1,35 @@
 ﻿using CICD_Core;
 
 Console.WriteLine("Enter the first Number");
-int a = int.Parse(Console.ReadLine());
 
+
+int a, b;
+while (true)
+{
+    string? s = Console.ReadLine();
+    if (!int.TryParse(s, out a))
+    {
+        Console.WriteLine("Enter a integer");
+    }
+    else
+    {
+        break;
+    }
+}
 Console.WriteLine("Enter the second Number");
-int b = int.Parse(Console.ReadLine());
+while (true)
+{
+    string? s = Console.ReadLine();
+    if (!int.TryParse(s, out b))
+    {
+        Console.WriteLine("Enter a integer");
+    }
+    else
+    {
+        break;
+    }
+}
+
 
 double d = Calculator.Divide(a, b);
 Console.WriteLine($"{a} / {b} = {d}");
