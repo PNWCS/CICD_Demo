29117528a035666b5c350df371eac0c29297d076
Muhammad Kumail <mkumailofficial@gmail.com>
2025-02-10 13:51:41 -0600

    fix: static types issues

--- CICD_CLI/Program.cs
+++ CICD_CLI/Program.cs
@@ -1,10 +1,38 @@
 ﻿using CICD_Core;
 
 Console.WriteLine("Enter first number:");
-int a = int.Parse(Console.ReadLine());
+
+string? s = Console.ReadLine();
+int a, b;
+
+while (true)
+{
+  if (!int.TryParse(s, out a))
+  {
+    Console.WriteLine("Error: please enter an integer");
+    s = Console.ReadLine();
+
+  }
+  else
+  {
+    break;
+  }
+}
 
 Console.WriteLine("Enter second number:");
-int b = int.Parse(Console.ReadLine());
+
+while (true)
+{
+  s = Console.ReadLine();
+  if (!int.TryParse(s, out b))
+  {
+    Console.WriteLine("Error: please enter an integer");
+  }
+  else
+  {
+    break;
+  }
+}
 
 double result = Calculator.Divide(a, b);
 
