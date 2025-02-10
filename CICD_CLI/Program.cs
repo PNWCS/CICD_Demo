eea14c36123cd1dd7475c5603229b13d082b79bb
Mohammed-Shoaib124 <151385351+Mohammed-Shoaib124@users.noreply.github.com>
2025-02-10 19:19:28 +0000

    Added divide feature

--- CICD_CLI/Program.cs
+++ CICD_CLI/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using CICD_Core;
+
+Console.WriteLine("Enter the first Number");
+int a = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Enter the second Number");
+int b = int.Parse(Console.ReadLine());
+
+double d = Calculator.Divide(a, b);
+Console.WriteLine($"{a} / {b} = {d}");
---
+++ CICD_Core/Calculator.cs
@@ -0,0 +1,15 @@
+﻿
+namespace CICD_Core
+{
+    public class Calculator
+    {
+        public static double Divide(int a, int b)
+        {
+            if (b == 0)
+            {
+                throw new DivideByZeroException();
+            }
+            return (double)a / (double)b;
+        }
+    }
+}
