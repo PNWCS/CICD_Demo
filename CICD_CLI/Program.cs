5f28fd140c5294987f69d0057d5eab6f28f832f2
Muhammad Kumail <mkumailofficial@gmail.com>
2025-02-10 13:15:47 -0600

    feat: ci/cd

--- CICD_CLI/Program.cs
+++ CICD_CLI/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using CICD_Core;
+
+Console.WriteLine("Enter first number:");
+int a = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Enter second number:");
+int b = int.Parse(Console.ReadLine());
+
+double result = Calculator.Divide(a, b);
+
+Console.WriteLine($"{a} / {b} = {result}");
