ea7e998c68e8da8e02f0d3a553892feff3fc171b
Mohammed-Shoaib124 <151385351+Mohammed-Shoaib124@users.noreply.github.com>
2025-02-14 15:57:21 +0000

    updated

--- CICD_CLI/Program.cs
+++ CICD_CLI/Program.cs
@@ -1,10 +1,6 @@
-
 ﻿using System.ComponentModel;
 using CICD_Core;
-
 Console.WriteLine("Enter the first Number");
-
-
 int a, b;
 while (true)
 {
@@ -19,7 +15,6 @@ while (true)
     }
 }
 Console.WriteLine("Enter the second Number");
-
 while (true)
 {
     string? s = Console.ReadLine();
@@ -32,7 +27,5 @@ while (true)
         break;
     }
 }
-
-
 double d = Calculator.Divide(a, b);
 Console.WriteLine($"{a} / {b} = {d}");
